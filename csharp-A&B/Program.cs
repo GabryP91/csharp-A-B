1d57cac5f130529a166bb63bb4ee7826e801ca86
gabry <gabry@GABRY1>
2024-12-20 17:21:54 +0100

    Aggiungere i file di progetto.

---
+++ csharp-A&B/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_A_B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
